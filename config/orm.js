220547b72c9d0041abf98f047e408ee7ab43e61c
Zaya <zayazaw67@gmail.com>
2019-10-17 21:27:41 -0500

    testing for live page

--- config/orm.js
+++ config/orm.js
@@ -36,7 +36,7 @@ var orm = {
 	selectAll: function(tableInput, cb) {
 		var queryString = "SELECT * FROM " + tableInput + ";";
 		connection.query(queryString, function(err, result) {
-			if (err) {throw err};
+			if (err) throw err;
 			cb(result);
 		});
 	},
